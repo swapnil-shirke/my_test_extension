54ef709d7efdd07f242835cb166c3cb5ebe3dc18
swapnil-shirke <swapnil.shirke@raweng.com>
2017-03-02 17:42:40 +0530

    add application info and keys

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 var Built = require('built-extension-sdk')
 
 // Initiate application
-var app = Built.App('blt917c46b3de8e419f')
+var app = Built.App('blt9407dfdad47b47aa')
 // .setHost('localhost')
 // .setPort(8000)
 .setHost("dev-api.built.io")
 .setProtocol("https")
 // .setPort(80)
-.setMasterKey('blte09616313f4eaf7a')
+.setMasterKey('blt45451d69ab2dcb65')
 
 var extensionSDK = app.Extension({
-	secret_key     : 'blt917c46b3de8e419f',
+	secret_key     : 'blt6fc5a562f0de9b14',
 	extension_key	 : 'blt_ext_default',
 	static         : __dirname + '/client',
 	routes         : require('./server/routes')
