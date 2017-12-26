f89c3c18b763855c67fd5221da70153a32f70af2
swapnil-shirke <swapnil.shirke@raweng.com>
2017-12-26 12:54:16 +0530

    Update app.js
    adds new application data

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 var Built = require('built-extension-sdk')
 
 // Initiate application
-var app = Built.App('blt9407dfdad47b47aa')
+var app = Built.App('blt8f9dff6a8e3d329c')
 // .setHost('localhost')
 // .setPort(8000)
 .setHost("dev-api.built.io")
 .setProtocol("https")
 // .setPort(80)
-.setMasterKey('blt45451d69ab2dcb65')
+.setMasterKey('blt89bb8dd35204a8dc')
 
 var extensionSDK = app.Extension({
-	secret_key     : 'blt6fc5a562f0de9b14',
+	secret_key     : 'blt5c030ae69e6c219b',
 	extension_key	 : 'blt_ext_default',
 	static         : __dirname + '/client',
 	routes         : require('./server/routes')
