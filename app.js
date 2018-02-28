17c318802a6415593557bd43958cad4e895cd19d
swapnil-shirke <swapnil.shirke@raweng.com>
2018-02-28 14:47:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 var Built = require('built-extension-sdk')
 
 // Initiate application
-var app = Built.App('blt9e750c9042a275c0')
+var app = Built.App('blt9e750c9042a275')
 // .setHost('localhost')
 // .setPort(8000)
 .setHost("stag-api.built.io")
 .setProtocol("https")
 // .setPort(80)
-.setMasterKey('blt00e5c52836f7ecb3')
+.setMasterKey('blt00e5c52836f7ec')
 
 var extensionSDK = app.Extension({
-	secret_key     : 'mojojojo',
+	secret_key     : 'mojojogo',
 	extension_key	 : 'blt_ext_default',
 	static         : __dirname + '/client',
 	routes         : require('./server/routes')
